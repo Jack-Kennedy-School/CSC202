bc4ef8a08daf37897f81b3c7094278f23f7ec95d
Jack-Kennedy-School <jfkowe@gmail.com>
2023-05-01 12:19:57 -0400

    Chapter 12

---
+++ EqualCard.cpp
@@ -0,0 +1,4 @@
+bool equals(const Card& c1, const Card& c2)
+{
+    return (c1.rank == c2.rank && c1.suit == c2.suit);
+}
---
+++ FullDeck.cpp
@@ -0,0 +1,13 @@
+vector<Card> build_deck()
+{
+    vector<Card> deck(52);
+    int i = 0;
+    for (int suit = 0; suit <= 3; suit++) {
+        for (int rank = 1; rank <= 13; rank++) {
+            deck[i].suit = suit;
+            deck[i].rank = rank;
+            i++;
+        }
+    }
+    return deck;
+}
